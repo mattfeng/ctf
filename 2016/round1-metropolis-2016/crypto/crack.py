7fb7a815520fb779bd858a347cf25fd40e54b663
Matthew Feng <netsec.phs@gmail.com>
2016-03-30 11:22:40 -0400

    implement blindhide algorithm

--- 2016/round1-metropolis-2016/crypto/crack.py
+++ 2016/round1-metropolis-2016/crypto/crack.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python
+
+# implements the blind hide algorithm with no password
+
+from PIL import *
+from PIL import Image
 import hackercodecs
 
+mas = Image.open('masked.bmp')
 
-string = "M29kt21ntaoyKKvlAYFnteFdue5dRUTzQe91vUTrROF1ued0wK4yN2zdtaT3RKTdtOXhtUTdQOFgAY90sYNqDUT3RKTmRONcAZJnAZNrRKTzAYFnRYMm"
+width, height = 650, 650
 
-plaintext =
-"U29sb21vbiwgSSdtIGNvbmNlcm5lZCBhYm91dCBzZWN1cml0eS4gV2hlbiB3ZSBlbWFpbCBlYWNo\nIG90aGVyLCB3ZSBuZWVkIHRvIHVzZSBhIGNvZGUu"
+message = ''
 
-for i in range(1, 26):
-    dec = string.decode('base64')
+for y in range(0, height):
+    for x in range(0, width):
+        pix = mas.getpixel((x, y))
+        message += str(bin(pix[2]))[-1]
+        message += str(bin(pix[1]))[-1]
+        message += str(bin(pix[0]))[-1]
 
-    try:
-
-        print dec.decode('rot' + str(i))
-
-    except:
-        pass
-
+print message[:8*60].decode('bin')
